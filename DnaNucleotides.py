68f1f403e34f9b316ec4d9c7ed1c5fcd47364854
bolebop022 <bolebop022@student.wethinkcode.co.za>
2023-07-07 21:56:53 +0200

    transcribing function added.

--- DnaNucleotides.py
+++ DnaNucleotides.py
@@ -10,5 +10,8 @@ def count_nucleotides(nucleotides):
     return (nucleotides.count('A'), nucleotides.count('C')
             ,nucleotides.count('G'),nucleotides.count('T'))
 
+def transcribing_DNA_to_RNA(nucleotides):
+    return nucleotides.replace('T','U')
+
 if __name__ == "__main__":
-    print(*count_nucleotides(read_file("Nucleotide.txt")))
+    print(transcribing_DNA_to_RNA(read_file("Nucleotide.txt")))
