1e3a2763fdfaa0dfa7f3e555d30cf4f3e60e1b4e
bolebop022 <bolebop022@student.wethinkcode.co.za>
2023-07-07 22:21:09 +0200

    Translation

--- DnaNucleotides.py
+++ DnaNucleotides.py
@@ -13,5 +13,21 @@ def count_nucleotides(nucleotides):
 def transcribing_DNA_to_RNA(nucleotides):
     return nucleotides.replace('T','U')
 
+
+def reverse_C_of_DNA(nucleotides):
+    new_nucleotides = ""
+    for i in range(0, len(nucleotides)):
+        if nucleotides[i] == 'A':
+            new_nucleotides = f'{new_nucleotides}T'
+        elif nucleotides[i] == 'T':
+            new_nucleotides = f'{new_nucleotides}A'
+        elif nucleotides[i] == 'G':
+            new_nucleotides = f'{new_nucleotides}C'
+        elif nucleotides[i] == 'C':
+            new_nucleotides = f'{new_nucleotides}G'
+
+    return new_nucleotides[::-1]
+    
+
 if __name__ == "__main__":
-    print(transcribing_DNA_to_RNA(read_file("Nucleotide.txt")))
+    print(reverse_C_of_DNA(read_file("Nucleotide.txt")))
